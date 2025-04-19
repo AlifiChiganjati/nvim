def4834066f0a79edd2259d0e6c1f34b4ebc0d66
Alifi Chiganjati <alifichiganjati@gmail.com>
2025-04-19 19:35:48 +0800

    FEAT: first commit

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    -- Add the Laravel.nvim plugin which gives the ability to run Artisan commands
+    -- from Neovim.
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    ft = { "php" },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    config = true,
+    opts = {
+      lsp_server = "intelephense",
+      features = {
+        -- null_ls = { enable = false },
+        pickers = {
+          provider = "fzf-lua",
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,13 @@
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = function(_, opts)
+    opts.linters_by_ft = {
+      dockerfile = { "hadolint" },
+      markdown = { "markdownlint-cli2" },
+      php = { "phpstan" },
+      sql = { "sqlfluff" },
+    }
+    opts.linters = {}
+  end,
+}
